6be65ee782afabf33c2a336630d2c6605b067e6f
PIYUSH WADHAWAN <wa.piyush@gmail.com>
2023-10-02 19:16:52 +0530

    added add/delete/filter and local storage

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
 const itemForm = document.querySelector("#item-form");
 const itemInput = document.querySelector("#item-input");
 const itemList = document.querySelector("#item-list");
+const clearAllBtn = document.querySelector(".btn-clear");
+const items = itemList.querySelectorAll("li");
+const itemFilter = document.querySelector(".form-input-filter");
 
-const addItem = (e) => {
+const getItemsFromStorage = () => {
+  let itemsFromStorage;
+  if (localStorage.getItem("items") === null) {
+    itemsFromStorage = [];
+  } else {
+    itemsFromStorage = JSON.parse(localStorage.getItem("items"));
+  }
+  return itemsFromStorage;
+};
+
+const displayItems = () => {
+  itemsFromStorage = getItemsFromStorage();
+  itemsFromStorage.forEach((item) => addItemToDom(item));
+  checkUI();
+};
+
+const addItemSubmit = (e) => {
   e.preventDefault();
   if (itemInput.value === "") {
     alert("Please enter item name");
   } else {
-    const newItem = document.createElement("li");
-    newItem.appendChild(document.createTextNode(itemInput.value));
-    const button = createButton("remove-item btn-link text-red");
-    newItem.appendChild(button);
-    itemList.appendChild(newItem);
+    addItemToDom(itemInput.value);
+    addItemToStorage(itemInput.value);
     itemInput.value = "";
+    checkUI();
   }
+};
+
+const addItemToDom = (item) => {
+  const newItem = document.createElement("li");
+  newItem.appendChild(document.createTextNode(item));
+  const button = createButton("remove-item btn-link text-red");
+  newItem.appendChild(button);
+  itemList.appendChild(newItem);
+};
+
+const addItemToStorage = (item) => {
+  itemsFromStorage = getItemsFromStorage();
+  itemsFromStorage.push(item);
+  localStorage.setItem("items", JSON.stringify(itemsFromStorage));
 };
 
 const createButton = (classes) => {
@@ -22,16 +53,70 @@ const createButton = (classes) => {
   const icon = document.createElement("i");
   icon.className = "fa-solid fa-xmark";
   newButton.appendChild(icon);
-  console.log(newButton);
   return newButton;
 };
 
 const removeItem = (e) => {
   if (e.target.nodeName === "I") {
-    const element = e.target.parentElement.parentElement;
-    element.remove();
+    if (confirm("Are you sure?")) {
+      const element = e.target.parentElement.parentElement;
+      element.remove();
+    }
+    checkUI();
   }
 };
 
-itemForm.addEventListener("submit", addItem);
+const clearAllItems = (e) => {
+  if (confirm("Delete all?")) {
+    while (itemList.firstChild) {
+      itemList.firstChild.remove();
+    }
+    checkUI();
+  }
+};
+
+const checkUI = () => {
+  const items = itemList.querySelectorAll("li");
+  if (items.length === 0) {
+    clearAllBtn.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clearAllBtn.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+};
+
+const filterItems = () => {
+  items.forEach((item) => {
+    if (
+      !item.innerText.toLowerCase().includes(itemFilter.value.toLowerCase())
+    ) {
+      item.style.display = "none";
+    } else {
+      item.style.display = "block";
+    }
+  });
+};
+
+itemForm.addEventListener("submit", addItemSubmit);
 itemList.addEventListener("click", removeItem);
+clearAllBtn.addEventListener("click", clearAllItems);
+itemFilter.addEventListener("input", filterItems);
+document.addEventListener("DOMContentLoaded", displayItems);
+
+checkUI();
+
+// localStorage and sessionStorage
+// property on Window interface that allows us to access Storage object
+// data stored in key/value pair, value are strings, cannot store objects
+// localStorage does not expire while sessionStorage only last till page closed
+
+// // Helpfull methods
+// // set a value with a key
+// localStorage.setItem("name", "Brad");
+// // get a value using the key
+// localStorage.getItem("name");
+// // remove item using the key
+// localStorage.removeItem("name");
+// // clear all values
+// localStorage.clear();
